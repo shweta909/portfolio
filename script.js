003aa01a1125ea7ef98a3e99cb072205d8181660
shweta909 <shwetadhoble126@gmail.com>
2026-06-29 20:52:58 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,47 @@
+const roles = [
+    "Full Stack Developer",
+    "MERN Stack Developer",
+    "Java Developer",
+    "Python Enthusiast",
+    "SQL Developer"
+];
 
+let roleIndex = 0;
+let charIndex = 0;
+let currentRole = "";
+let isDeleting = false;
+
+function type() {
+
+    currentRole = roles[roleIndex];
+
+    if (!isDeleting) {
+
+        document.getElementById("typing").textContent =
+            currentRole.substring(0, charIndex++);
+
+        if (charIndex > currentRole.length) {
+            isDeleting = true;
+            setTimeout(type, 1200);
+            return;
+        }
+
+    } else {
+
+        document.getElementById("typing").textContent =
+            currentRole.substring(0, charIndex--);
+
+        if (charIndex === 0) {
+            isDeleting = false;
+            roleIndex++;
+
+            if (roleIndex >= roles.length)
+                roleIndex = 0;
+        }
+
+    }
+
+    setTimeout(type, isDeleting ? 50 : 100);
+}
+
+type();
